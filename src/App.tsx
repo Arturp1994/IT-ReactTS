02cebe32a94bf6466c4420dbda086a0397ca0cab
Arthur <arturiopr1994@gmail.com>
2022-06-08 13:12:37 +0300

    06

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordio";
 import {Rating} from "./components/Reting";
+import OnOff from "./components/OnOff";
 
 function App() {
   return (
     <div className="App">
-        <PageTitle title={'Заголовок1'}/>
-        <PageTitle title={'Заголовок2'}/>
+        <OnOff/>
+        <OnOff/>
+        <OnOff/>
+        <OnOff/>
+        {/*<PageTitle title={'Заголовок1'}/>*/}
+        {/*<PageTitle title={'Заголовок2'}/>*/}
 
-        <Accordion titleValue = {'Меню1'} collapsed ={true}/>
-        <Accordion titleValue = {'Меню2'} collapsed ={false}/>
+        {/*<Accordion titleValue = {'Меню1'} collapsed ={true}/>*/}
+        {/*<Accordion titleValue = {'Меню2'} collapsed ={false}/>*/}
 
-        <Rating value = {1}/>
-        <Rating value = {2}/>
-        <Rating value = {3}/>
-        <Rating value = {4}/>
-        <Rating value = {5}/>
+        {/*<Rating value = {1}/>*/}
+        {/*<Rating value = {2}/>*/}
+        {/*<Rating value = {3}/>*/}
+        {/*<Rating value = {4}/>*/}
+        {/*<Rating value = {5}/>*/}
     </div>
   );
 }
